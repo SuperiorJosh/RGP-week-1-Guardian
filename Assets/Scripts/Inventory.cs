1b5f966d734540ad2c6cbb06e5135c6343cdcae7
Josh Taylor <josh@kyuu.nz>
2023-07-19 13:26:55 +1200

    Inventory block out, DOTween, Odin

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance { get; private set; }
+    
+    public void AddItem(ItemData item)
+    {
+        
+    }
+
+    public void RemoveItem(ItemData item)
+    {
+        
+    }
+}
