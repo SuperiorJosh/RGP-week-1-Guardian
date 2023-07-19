dfde2836db1491cedfa15a562eadffd0377c1a71
ICollins25 <103876198+ICollins25@users.noreply.github.com>
2023-07-19 14:44:43 +1200

    Usable

---
+++ Assets/Scripts/Usable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usable : MonoBehaviour
+{
+    Interactable interactable;
+    ItemData itemData;
+    
+    void Awake()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    void Start()
+    {
+        interactable.ItemInteraction.AddListener(UseObject);
+    }
+
+    void UseObject()
+    {
+        // Remove listener
+        interactable.ItemInteraction.RemoveListener(UseObject);
+        
+        // Add functionality here.
+
+        Destroy(gameObject);
+    }
+}
