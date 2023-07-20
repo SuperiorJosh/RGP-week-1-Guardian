df590dcdedb9f030272710b09ebb9c90ed78485a
Josh Stephens <jjrstephens@gmail.com>
2023-07-20 22:05:27 +1200

    Dialogue box Positioning

--- Assets/Scripts/Components/Dialogue.cs
+++ Assets/Scripts/Components/Dialogue.cs
@@ -8,6 +8,10 @@ public class Dialogue : MonoBehaviour
     // Variables
     [SerializeField] private string dialogueText;
     [SerializeField] private GameObject prefab;
+    [SerializeField] private float dialogueTimer;
+    [SerializeField] private GameObject UICanvas;
+    [SerializeField] private Vector3 dialoguePosition;
+    private GameObject dialogueBox = null;
 
     // References
     private Interactable interactableComponent;
@@ -20,10 +24,23 @@ public class Dialogue : MonoBehaviour
 
     private void OnInteract(ItemData _heldItemData)
     {
-        // Instantiate dialogue box and get reference to text component
-        GameObject dialogueBox = Instantiate(prefab);
-        TMP_Text textBox = dialogueBox.GetComponentInChildren<TMP_Text>();
+        if (dialogueBox == null)
+        {
+            // Instantiate dialogue box and get reference to text component
+            dialogueBox = Instantiate(prefab);
+            dialogueBox.transform.SetParent(UICanvas.transform, false);
+            dialogueBox.transform.position = dialoguePosition;
+            TMP_Text textBox = dialogueBox.GetComponentInChildren<TMP_Text>();
+            textBox.text = dialogueText;
 
-        textBox.text = dialogueText;
+            StartCoroutine(TimerForDestroy());
+        }
+    }
+
+    private IEnumerator TimerForDestroy()
+    {
+        yield return new WaitForSeconds(dialogueTimer);
+
+        Destroy(dialogueBox);
     }
 }
