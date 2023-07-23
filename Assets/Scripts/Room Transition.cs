7f1f95aa3251a3d4554202d11ca43bb090479fa0
ICollins25 <103876198+ICollins25@users.noreply.github.com>
2023-07-23 22:03:37 +1200

    Game step testing on TV

--- Assets/Scripts/Room Transition.cs
+++ Assets/Scripts/Room Transition.cs
@@ -7,6 +7,7 @@ public class RoomTransition : MonoBehaviour
     // Variables
     [SerializeField] private Vector3 newPosition;
     [SerializeField] private Quaternion newRotation;
+    [SerializeField] GameStepEvent gameStep;
 
     // References
     private GameObject mainCamera;
@@ -21,6 +22,12 @@ public class RoomTransition : MonoBehaviour
     // Change Rooms
     public void ChangeRoom()
     {
+        // Check game state here, is it valid to swap rooms yet?
+        if(gameStep.CurrentState != GameStepEventState.Completed)
+        {
+            return;
+        }
+
         mainCamera.transform.position = newPosition;
         mainCamera.transform.rotation = newRotation;
     }
