917e04462796dbaecae0ad4ce429f1791a23e099
ICollins25 <103876198+ICollins25@users.noreply.github.com>
2023-07-26 11:35:55 +1200

    Rotating tenant

--- Assets/Scripts/Components/RotateBillboard.cs
+++ Assets/Scripts/Components/RotateBillboard.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class RotateBillboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject mainCamera;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float _camYrotation = mainCamera.transform.rotation.y;
+        float _angleDifference = _camYrotation - gameObject.transform.rotation.y;
+
+        gameObject.transform.Rotate(0.0f, _angleDifference, 0.0f, Space.World);
+        //Debug.Log("Cam Y rotation: " + _camYrotation);
     }
 }
