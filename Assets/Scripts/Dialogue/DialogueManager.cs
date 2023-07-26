7fcb721fefe25b88525a0b569357974a0238479a
Josh Taylor <josh@kyuu.nz>
2023-07-26 14:23:27 +1200

    Dialogue box changes

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -8,10 +8,8 @@ public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance { get; private set; }
     
-    [SerializeField]GameObject dialogueUI;
-    [SerializeField] Image speakersImage;
-    [SerializeField] TMP_Text tmpro;
-    private int linesIndex;
+    [SerializeField] private UIDialogue m_dialogueBox;
+
     DialogueData dialogueReceived;    
 
     private void Awake()
@@ -29,42 +27,23 @@ public class DialogueManager : MonoBehaviour
 
     public void ProcessDialogue(DialogueData _dialogueReceived)
     {
-        if(_dialogueReceived.dialogueDataList.Count == 0)
+        if (_dialogueReceived.dialogueDataList.Count == 0)
         {
             // No lines provided. Do nothing.
             return;
         }
         
-        // Assign sprite and first line.
-        dialogueUI.SetActive(true);
-        linesIndex = 0;
-        dialogueReceived = _dialogueReceived;
-        var speaker = _dialogueReceived.dialogueDataList[0].Speaker;
-        speakersImage.sprite = speaker.GetEmotion(_dialogueReceived.dialogueDataList[0].Emotion);
-        tmpro.text = dialogueReceived.dialogueDataList[0].dialogueLine;
-
-        // TODO: Activate dialogue UI.
+        m_dialogueBox.ShowDialogue(_dialogueReceived);
     }
 
     public void NextLine()
     {
-        linesIndex++;
-        if(linesIndex < dialogueReceived.dialogueDataList.Count)
-        {
-            var dialogue = dialogueReceived.dialogueDataList[linesIndex];
-            var speaker = dialogueReceived.dialogueDataList[linesIndex].Speaker;
-            speakersImage.sprite = speaker.GetEmotion(dialogue.Emotion);
-            tmpro.text = dialogue.dialogueLine;
-        }
-        else{
-            // No more lines.
-            // TODO: Deactivate dialogue UI.
-            dialogueUI.SetActive(false);
-        }
+        m_dialogueBox.NextLine();
     }
+
 
     public bool IsUIShowing()
     {
-        return dialogueUI.activeSelf;
+        return m_dialogueBox.IsActive;
     }
 }
--- Assets/UIDialogue.cs
+++ Assets/UIDialogue.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -10,10 +13,87 @@ public class UIDialogue : MonoBehaviour
     [SerializeField] private Image m_speakerImage;
     [SerializeField] private TMP_Text m_text;
 
+    private CanvasGroup m_canvasGroup;
+
     public UnityEvent DialogueFinished;
+
+    private DialogueSpeaker m_currentSpeaker;
+    private DialogueData m_data;
+    private int linesIndex;
+
+    private RectTransform m_rectTransform;
+    [SerializeField] private RectTransform m_textPanel;
+
+    public bool IsActive => m_data != null;
+    
+    private void Awake()
+    {
+        m_canvasGroup = GetComponent<CanvasGroup>();
+        m_rectTransform = transform as RectTransform;
+    }
+
+    private void Start()
+    {
+        m_canvasGroup.alpha = 0;
+        m_canvasGroup.blocksRaycasts = false;
+        m_text.text = "";
+        m_textPanel.localScale = new Vector3(0, 0, 0);
+        m_speakerImage.transform.localScale = new Vector3(0, 0, 0);
+    }
 
     public void ShowDialogue(DialogueData dialogue)
     {
+        linesIndex = 0;
+        m_canvasGroup.blocksRaycasts = true;
         
+        m_data = dialogue;
+        var firstDialogue = m_data.dialogueDataList[0];
+        m_currentSpeaker = firstDialogue.Speaker;
+        m_speakerImage.sprite = firstDialogue.Speaker.GetEmotion(firstDialogue.Emotion);
+        var sequence = DOTween.Sequence()
+            .Insert(0f, m_speakerImage.transform.DOScale(new Vector3(1f, 1f, 1f), 0.2f))
+            .Insert(0.1f, m_canvasGroup.DOFade(1f, 0.2f))
+            .Insert(0.1f, m_textPanel.DOScale(new Vector3(1f, 1f, 1f), 0.5f));
+
+        sequence.OnComplete(() =>
+        {
+            m_text.text = firstDialogue.dialogueLine;
+            linesIndex++;
+        });
+        
+        sequence.Play();
+    }
+
+    private void ShowNext()
+    {
+        linesIndex++;
+        if (linesIndex < m_data.dialogueDataList.Count)
+        {
+            var next = m_data.dialogueDataList[linesIndex];
+            if (next.Speaker != null && next.Speaker != m_currentSpeaker)
+            {
+                m_currentSpeaker = next.Speaker;
+            }
+            m_speakerImage.sprite = m_currentSpeaker.GetEmotion(next.Emotion);
+            m_text.text = next.dialogueLine;
+        }
+        else
+        {
+            var sequence = DOTween.Sequence();
+            sequence.Insert(0, m_rectTransform.DOScale(new Vector3(0f, 0f, 0f), 0.5f));
+            sequence.Insert(0.3f, m_canvasGroup.DOFade(0f, 0.2f));
+            sequence.OnComplete(() =>
+            {
+                m_canvasGroup.alpha = 0;
+                m_canvasGroup.blocksRaycasts = false;
+                m_data = null;
+                DialogueFinished?.Invoke();
+            });
+        }
+    }
+    
+    public void NextLine()
+    {
+        ShowNext();
     }
 }
